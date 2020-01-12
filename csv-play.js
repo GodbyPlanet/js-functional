07eccfce7fce7a042b5758a948163e6830ae455c
Nemanja <nemanja.vasic@codecentric.de>
2020-01-12 12:53:49 +0100

    Add functional code

--- csv-play.js
+++ csv-play.js
@@ -39,6 +39,8 @@ const getMostProfitableVegetablesRecord = getVegetablesRecords.reduce(
 // console.log(getVegetablesRecords);
 // console.log(getMostProfitableVegetablesRecord);
 
+// FUNCTIONAL PART
+
 // const split = reg => data => data.split(reg);
 // const splitData = split("\r\n");
 // console.log(splitData(getData()));
@@ -46,10 +48,51 @@ const getMostProfitableVegetablesRecord = getVegetablesRecords.reduce(
 const partial = (fn, ...args) => (...restArgs) => fn(...args, ...restArgs);
 const split = (reg, data) => data.split(reg);
 const slice = (from, to, xs) => xs.slice(from, to);
+const map = (fn, xs) => xs.map(fn);
+const filter = (fn, xs) => xs.filter(fn);
+const reduce = (fn, xs) => xs.reduce(fn);
 
 const splitSalesRecordsByNewLine = partial(split, "\r\n");
-// console.log(splitSalesRecordsByNewLine(getData()));
+const splitedByNewLine = splitSalesRecordsByNewLine(getData());
+const splitRecordsBySemiColon = partial(split, ";");
+const mapAndSplitBySemiColon = partial(map, record =>
+  splitRecordsBySemiColon(record)
+);
+const splitedRecordsBySemiColon = mapAndSplitBySemiColon(splitedByNewLine);
+const mapRecordToObject = item => ({
+  region: item[0],
+  country: item[1],
+  itemType: item[2],
+  salesChannel: item[3],
+  orderPriority: item[4],
+  orderDate: item[5],
+  orderID: item[6],
+  shipDate: item[7],
+  unitsSold: item[8],
+  unitPrice: item[9],
+  unitCost: item[10],
+  totalRevenue: item[11],
+  totalCost: item[12],
+  totalProfit: item[13]
+});
+const mapppedRecordsToObjects = map(
+  record => mapRecordToObject(record),
+  splitedRecordsBySemiColon
+);
+const firstTenRecords = partial(slice, 1, 11);
 
-const records = splitSalesRecordsByNewLine(getData());
-const firstTenRecords = slice(1, 11, records);
-console.log(firstTenRecords);
+// console.log(splitedRecordsBySemiColon);
+// console.log(mapppedRecordsToObjects);
+// console.log(firstTenRecords(mapppedRecordsToObjects));
+
+const getVegetablesRecordsFP = filter(
+  record => record.itemType === "Vegetables",
+  firstTenRecords(mapppedRecordsToObjects)
+);
+console.log(getVegetablesRecordsFP);
+
+const getMostProfitableVegetablesRecordFP = reduce(
+  (prev, curr) => (prev.totalProfit > curr.totalProfit ? prev : curr),
+  getVegetablesRecordsFP
+);
+console.log(getMostProfitableVegetablesRecordFP);
